fdc9d1b01aebe1beb8fc6de8bbc09f751f6a86f7
wgd <wgd@qq.com>
2016-05-19 14:14:21 +0800

    点击头像可出现用户详情

--- tdengine_ddz/src/ui/user_detail_info.lua
+++ tdengine_ddz/src/ui/user_detail_info.lua
@@ -29,16 +29,32 @@ function USER_DETEAIL_INFO_CLASS:init_ui()
     close_btn:addTouchEventListener(touchEvent)
     self:addChild(close_btn)
 
-    self.account = cc.Label:createWithSystemFont("账号:", "Arial", 20):move(100, 50):addTo(self)
-    self.account:setAnchorPoint(cc.p(0.5, 0.5))
+    -- self.account = cc.Label:createWithSystemFont("账号:", "Arial", 20):move(100, 50):addTo(self)
+    -- self.account:setAnchorPoint(cc.p(0.5, 0.5))
 
 
-    self.account_detail = cc.Label:createWithSystemFont("", "Arial", 20):move(200, 50):addTo(self)
-    self.account_detail:setAnchorPoint(cc.p(0, 0.5))
+    -- self.account_detail = cc.Label:createWithSystemFont("", "Arial", 20):move(200, 50):addTo(self)
+    -- self.account_detail:setAnchorPoint(cc.p(0, 0.5))
+
+    self.all_detail = cc.Label:createWithSystemFont("", "Arial", 20):move(200, 50):addTo(self)
+    self.all_detail:setContentSize(self:getContentSize())
+    self.all_detail:setAnchorPoint(cc.p(0, 1))
+    self.all_detail:setPosition(cc.p(0, self:getContentSize().height))
+end
+
+function USER_DETEAIL_INFO_CLASS:get_all_desc()
+    local data = self.data
+    local ddz_info = data.ddz_info
+    trace("data is %o", data)
+    return "名称:" .. (data.name or "") .. 
+           "\n胜场:" .. tostring(ddz_info.win_amount or 0) .. 
+           "\n负场:" .. tostring(ddz_info.lose_amount or 0) .. 
+           "\n豆豆:" .. tostring(ddz_info.pea_amount or 0)
 end
 
 function USER_DETEAIL_INFO_CLASS:set_data(data)
-    self.account_detail:setString(data.name or "xxxx")
+    self.data = data
+    self.all_detail:setString(self:get_all_desc())
 end
 
 
